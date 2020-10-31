0fabd51641484bb7d07930881d6d63de836801e8
anacmolina <anac.molina@udea.edu.co>
2020-10-30 22:56:18 -0500

    first update code

---
+++ Parcial III/fd_nonlinear.cpp
@@ -0,0 +1,11 @@
+//================================================
+// Codigo de diferencias finitas no lineares     |
+//                                               |    
+// Burden Sec 11.4 -- Fisica computacional II    | 
+//                                               |  
+// Andres Arteaga                                |  
+// Ana Cristina Molina                           |
+//                                               |   
+// Update: 30 de octubre de 2020                 |
+//================================================
+
